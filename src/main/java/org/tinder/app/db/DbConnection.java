c030d46dd62a5ee498ede23dd34f48e9eada5d46
Müzəffər Əliyev <muzeffereli@gmail.com>
2022-10-30 20:00:57 +0400

    COMMIT

--- src/main/java/org/tinder/app/db/DbConnection.java
+++ src/main/java/org/tinder/app/db/DbConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
-    private final static String URL = "jdbc:postgres://nglxiuekikrqwe:e36c325b1e86cb17efa754ba765e8e0ce5d40e6e8e339832dcc0c9c787cf0f85@ec2-44-199-22-207.compute-1.amazonaws.com:5432/ddmnm5bm93ii53";
-    private final static String NAME = "nglxiuekikrqwe";
-    private final static String PWD = "e36c325b1e86cb17efa754ba765e8e0ce5d40e6e8e339832dcc0c9c787cf0f85";
+    private final static String URL = "postgres://ertwcqouskognp:fe5435ead3547487eb79c0dfd7d42ec0dec7e17e85ed4b4468f5713602de4dc4@ec2-52-23-131-232.compute-1.amazonaws.com:5432/ddohpjksno9atv";
+    private final static String NAME = "ertwcqouskognp";
+    private final static String PWD = "fe5435ead3547487eb79c0dfd7d42ec0dec7e17e85ed4b4468f5713602de4dc4";
     private Connection connection=null;
 
 
